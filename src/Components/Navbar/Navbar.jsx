8ebaf652edfb94a78339a0577a8d22d3c8bde217
tanvii-18 <8438tanvipatel@gmail.com>
2025-09-01 11:16:52 +0530

    01-09-25

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,11 +3,13 @@ import { NavLink } from "react-router-dom";
 import { GoHome } from "react-icons/go";
 import { PiBooksLight } from "react-icons/pi";
 import { TiGroup } from "react-icons/ti";
+import { FiLogIn } from "react-icons/fi";
 
 const NavLinkData = [
   { id: 1, path: "/", icon: <GoHome />, text: "Home" },
   { id: 2, path: "/books", icon: <PiBooksLight />, text: "books" },
   { id: 3, path: "/members", icon: <TiGroup />, text: "members" },
+  { id: 4, path: "/logIn", icon: <FiLogIn />, text: "LogIn" },
 ];
 
 const Navbar = () => {
