11dc6732a66b18fcc43173c923c098f39a3b5b4e
tanvii-18 <8438tanvipatel@gmail.com>
2025-09-03 11:08:42 +0530

    03-09-25

--- src/Components/Auth/Login.jsx
+++ src/Components/Auth/Login.jsx
@@ -1,59 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Auth/login.css";
 import { Link } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
-// import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { app } from "../firebase/firebase";
+
+const provider = new GoogleAuthProvider();
+const auth = getAuth(app);
 
 function Login() {
-  // const auth = getAuth();
-  // signInWithEmailAndPassword(auth, email, password)
-  //   .then((userCredential) => {
-  //     const user = userCredential.user;
-  //   })
-  //   .catch((error) => {
-  //     const errorCode = error.code;
-  //     const errorMessage = error.message;
-  //   });
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignIn = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then(() => alert("user login successful !!"))
+      .catch((err) => alert("user login failed ! " + err));
+  };
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        alert("Sign-out successful");
+      })
+      .catch((error) => {
+        alert("An error happened." + error);
+      });
+  };
+
+  const handleSignInWithGoogle = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result.user.displayName);
+      })
+      .catch((err) => console.log("sign in failed " + err));
+  };
 
   return (
     <div className="main-logIn">
       <div className="login-container">
         {/* input fields for log in */}
-        <div className="info-login">
-          <div className="login-headings">
-            <h2>Log in to your Account</h2>
-            <p>Welcome back! Select method to log in:</p>
+        {!auth.currentUser ? (
+          <div className="info-login">
+            <div className="login-headings">
+              <h2>Log in to your Account</h2>
+              <p>Welcome back! Select method to log in:</p>
+            </div>
+
+            <div className="login-methods">
+              <button className="google-btn" onClick={handleSignInWithGoogle}>
+                <FcGoogle style={{ fontSize: "20px" }} />
+                Continue with Google
+              </button>
+            </div>
+
+            <div className="login-w-email">
+              <span>or continue with email</span>
+              <label htmlFor="email">
+                <input
+                  type="email"
+                  id="emailInput"
+                  placeholder="Email"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </label>
+              <label htmlFor="password">
+                <input
+                  type="password"
+                  id="passwordInput"
+                  placeholder="Password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </label>
+              <button className="login-Btn" onClick={handleSignIn}>
+                Log in
+              </button>
+              <p>
+                don’t have account? <Link to="/signup">Create Account</Link>
+              </p>
+            </div>
           </div>
-
-          <div className="login-methods">
-            <button className="google-btn">
-              <FcGoogle style={{ fontSize: "20px" }} />
-              Continue with Google
-            </button>
+        ) : (
+          <div className="log-Out">
+            <h3>
+              Welcome!
+              <p
+                style={{ fontSize: "18px", color: "black", fontWeight: "400" }}
+              >
+                {auth.currentUser.email}
+              </p>
+            </h3>
+            <button onClick={handleSignOut}>Log Out</button>
           </div>
-
-          <div className="login-w-email">
-            <span>or continue with email</span>
-
-            <label htmlFor="email">
-              <input type="email" name="" id="emailInput" placeholder="Email" />
-            </label>
-
-            <label htmlFor="password">
-              <input
-                type="password"
-                name=""
-                id="passwordInput"
-                placeholder="Password"
-              />
-            </label>
-
-            <button className="login-Btn">Log in</button>
-            <p>
-              don't have account? <Link to="/signup">Create Account</Link>
-            </p>
-          </div>
-        </div>
+        )}
 
         {/* side image related to login */}
         <div className="login-image">
@@ -67,5 +112,3 @@ function Login() {
 }
 
 export default Login;
-
-// Api key --> AIzaSyBKlhPox5WtqjApU1AwNdkjXUgLb4b87r4
